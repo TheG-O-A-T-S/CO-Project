ba00095bf1005acc75df8cc49cbcf259476bb3e9
cosmin-oros <oroscosmin09@gmail.com>
2023-03-25 19:41:07 +0200

    project structure

---
+++ Project/src/main/java/com/example/coproject/Main.java
@@ -0,0 +1,15 @@
+package com.example.coproject;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
