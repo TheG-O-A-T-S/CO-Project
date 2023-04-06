a8848dbdda1e87fb9e97c609f29aceb5e9354ee8
Liviuu1 <liviu.oprea@student.upt.ro>
2023-04-06 11:34:05 +0300

    encryption decryption

--- Project/src/main/java/com/example/coproject/cuda_algorithms/AlgoTesting.java
+++ Project/src/main/java/com/example/coproject/cuda_algorithms/AlgoTesting.java
@@ -2,6 +2,11 @@ package com.example.coproject.cuda_algorithms;
 
 public class AlgoTesting {
     public static void main(String[] args) {
+        CryptographyBenchmark benchmark1 = new CryptographyBenchmark();
+        MemoryBenchmark benchmark2 = new MemoryBenchmark();
+
+        benchmark1.computeEncryptionDecryption();
+        benchmark2.computeAccessTime(); 
 
     }
 }
