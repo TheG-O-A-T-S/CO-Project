83c065cb7c137b3fa8a641c2d0bc55015edd8687
Paly <paliciuc28@gmail.com>
2023-04-23 16:47:14 +0300

    gata testarile

---
+++ Project/src/main/java/com/example/coproject/gui/System_info.java
@@ -0,0 +1,11 @@
+package com.example.coproject.gui;
+
+public class System_info {
+    private static final String osName = System.getProperty("os.name");
+
+    public static void main(String[] args) {
+
+        System.out.println("osName =" + osName);
+
+    }
+}
