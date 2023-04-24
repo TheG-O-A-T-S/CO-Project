698d6232ea931d0a3101d86fb42cf898729610b5
Paly <paliciuc28@gmail.com>
2023-04-24 05:13:29 +0300

    Banner & footer

--- Project/src/main/java/com/example/coproject/gui/LandingPage.java
+++ Project/src/main/java/com/example/coproject/gui/LandingPage.java
@@ -17,29 +17,36 @@ public class LandingPage extends JFrame implements ActionListener {
         setPreferredSize(new Dimension(750, 600));
         // background color of the title 'The G.O.A.T.S ...'
         getRootPane().setBackground(new Color(95, 158, 160));
+        setResizable(false);
 
         algorithmButton1 = new JButton("Cryptography");
         algorithmButton1.setBackground(Color.GRAY);
         algorithmButton1.setOpaque(true);
+        algorithmButton1.setFocusable(false);
+        algorithmButton1.setVerticalAlignment(JButton.CENTER);
+
 
         algorithmButton2 = new JButton("Memory");
         algorithmButton2.setBackground(Color.GRAY);
         algorithmButton2.setOpaque(true);
+        algorithmButton2.setFocusable(false);
 
         // change this one to whatever
         testAlgorithmButton = new JButton("Test algorithm");
         testAlgorithmButton.addActionListener(this);
         testAlgorithmButton.setBackground(Color.GRAY);
         testAlgorithmButton.setOpaque(true);
+        testAlgorithmButton.setFocusable(false);
 
         //top pannel
         JPanel topPannel = new JPanel();
-        topPannel.setBackground(Color.magenta);
+        topPannel.setBackground(Color.black);
         topPannel.setPreferredSize(new Dimension(750,250));
 
         ImageIcon imageIcon = new ImageIcon("src/main/java/com/example/coproject/res/goattest_banner.png");
         Image image = imageIcon.getImage().getScaledInstance(750, 250, Image.SCALE_SMOOTH);
         topPannel.add(new JLabel(new ImageIcon(image)));
+
 
         add(topPannel,BorderLayout.NORTH);
 
@@ -52,26 +59,27 @@ public class LandingPage extends JFrame implements ActionListener {
         add(centerPanel,BorderLayout.CENTER);
 
         //bottom panel
-        JLabel bottomPannel = new JLabel();
+        JPanel bottomPannel = new JPanel();
         bottomPannel.setBackground(Color.green);
         add(bottomPannel,BorderLayout.SOUTH);
         bottomPannel.setPreferredSize(new Dimension(750,50));
 
         ImageIcon footer_pic = new ImageIcon("src/main/java/com/example/coproject/res/goattest_footer.png");
         Image image2 = footer_pic.getImage().getScaledInstance(750, 50, Image.SCALE_SMOOTH);
-        bottomPannel.add(new JLabel(new ImageIcon(image2)));
+
 
         JLabel bottom_info = new JLabel();
         bottom_info.setText("CPU: GPU: RAM: OS:");
         bottom_info.setHorizontalAlignment(JLabel.LEFT); //??????dc nu merge
+        bottom_info.setVerticalAlignment(JLabel.TOP);
         bottomPannel.add(bottom_info);
+        bottomPannel.add(new JLabel(new ImageIcon(image2)));
 
 
         // set background for the window
         getContentPane().setBackground(new Color(95, 158, 160));
 
         //set icon for window
-
         ImageIcon icon_logo = new ImageIcon("src/main/java/com/example/coproject/res/goattest_icon.png");
         setIconImage(icon_logo.getImage());
 
