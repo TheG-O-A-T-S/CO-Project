0cc966cdcabdcc975eb10cd3f53562acc4273c23
cosmin-oros <oroscosmin09@gmail.com>
2023-04-28 15:10:24 +0300

    string encryp decryp

--- Project/src/main/java/com/example/coproject/cuda_algorithms/CryptographyBenchmark.java
+++ Project/src/main/java/com/example/coproject/cuda_algorithms/CryptographyBenchmark.java
@@ -8,7 +8,7 @@ import java.security.*;
 import java.util.*;
 
 public class CryptographyBenchmark {
-    public long computeEncryptionDecryption1(){
+    public long computeEncryptionDecryption1(String inputString){
         JCudaDriver.setExceptionsEnabled(true);
         JCudaDriver.cuInit(0);
         CUdevice device = new CUdevice();
@@ -16,26 +16,45 @@ public class CryptographyBenchmark {
         CUcontext context = new CUcontext();
         JCudaDriver.cuCtxCreate(context, 0, device);
 
-        int dataSize = 10000000; // 10 million bytes
-        byte[] data = new byte[dataSize];
-        new Random().nextBytes(data);
+        String encryptedString = null;
 
-        long start = System.currentTimeMillis();
+        long start = System.nanoTime();
 
         try {
-            SecretKeySpec secretKey = new SecretKeySpec(new byte[16], "AES");
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            // Convert the input string to a byte array
+            byte[] inputData = inputString.getBytes();
+
+            // Generate a random 128-bit key
+            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+            keyGen.init(128);
+            SecretKey secretKey = keyGen.generateKey();
+
+            // Encrypt the input data using AES encryption
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
             cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            byte[] encrypted = cipher.doFinal(data);
+            byte[] encryptedData = cipher.doFinal(inputData);
+
+            // Convert the encrypted data to a string for output
+            encryptedString = new String(encryptedData);
+
+            // Decrypt the encrypted data using AES decryption
             cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            byte[] decrypted = cipher.doFinal(encrypted);
+            byte[] decryptedData = cipher.doFinal(encryptedData);
+
+            // Convert the decrypted data to a string for output
+            String decryptedString = new String(decryptedData);
+
+            // Verify that the decrypted data matches the original input
+            if (!decryptedString.equals(inputString)) {
+                System.err.println("Decryption failed");
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        long end = System.currentTimeMillis();
+        long end = System.nanoTime();
 
-        System.out.println("Time taken: " + (end - start) + "ms");
+        System.out.println("Time taken: " + (end - start) + "ns");
 
         JCudaDriver.cuCtxDestroy(context);
 
--- Project/src/main/java/com/example/coproject/gui/LandingPage.java
+++ Project/src/main/java/com/example/coproject/gui/LandingPage.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.security.InvalidKeyException;
 import  javax.sound.sampled.*;
 
 public class LandingPage extends JFrame implements ActionListener {
@@ -34,8 +35,9 @@ public class LandingPage extends JFrame implements ActionListener {
         algorithmButton1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
-                    long time = benchmarkTest1.computeEncryptionDecryption1();
-                    // transition to another screen
+                    // transition to another screen and require an input string
+                    String s = "Ana are mere";
+                    long time = benchmarkTest1.computeEncryptionDecryption1(s);
                 } catch (NullPointerException n) {
 
                 }
